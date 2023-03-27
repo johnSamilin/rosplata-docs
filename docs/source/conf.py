cc89cd5bbb95641ccd123ca3573da50f5c81466f
Alex Saltykov <johnSamilin@users.noreply.github.com>
2023-03-27 14:35:21 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Rosplata'
+copyright = '2022, Alexander Saltykov'
+author = 'Alexander Saltykov'
 
 release = '0.1'
 version = '0.1.0'
